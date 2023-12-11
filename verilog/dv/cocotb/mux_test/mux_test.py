f5e9710b965ec179bbb338a99abf2182725e0366
Anton Maurovic <anton@foogle.com>
2023-12-12 04:34:57 +1030

    gf180-test: Add simple check for expected values

--- verilog/dv/cocotb/mux_test/mux_test.py
+++ verilog/dv/cocotb/mux_test/mux_test.py
@@ -37,6 +37,11 @@ async def mux_test(dut):
     gpios_value_str = caravelEnv.monitor_gpio(37, 8).binstr
     cocotb.log.info (f"All gpios '{gpios_value_str}'")
     gpio_value_int = caravelEnv.monitor_gpio(31, 16).integer
+    expected = 0x55AA
+    if (gpio_value_int == expected):
+        cocotb.log.info (f"[TEST] Pass the gpio value is '{hex(gpio_value_int)}'")
+    else:
+        cocotb.log.error (f"[TEST] Fail the gpio value is :'{hex(gpio_value_int)}' expected {hex(expected)}")
 
     await caravelEnv.wait_mgmt_gpio(1)
     await caravelEnv.wait_mgmt_gpio(0)
@@ -44,12 +49,10 @@ async def mux_test(dut):
     gpios_value_str = caravelEnv.monitor_gpio(37, 8).binstr
     cocotb.log.info (f"All gpios '{gpios_value_str}'")
     gpio_value_int = caravelEnv.monitor_gpio(31, 16).integer
-
-    #expected_gpio_value = 0xF8
-    # expected_gpio_value = 0x8F
-    # if (gpio_value_int == expected_gpio_value):
-    #     cocotb.log.info (f"[TEST] Pass the gpio value is '{hex(gpio_value_int)}'")
-    # else:
-    #     cocotb.log.error (f"[TEST] Fail the gpio value is :'{hex(gpio_value_int)}' expected {hex(expected_gpio_value)}")
+    expected = 0xAA55
+    if (gpio_value_int == expected):
+        cocotb.log.info (f"[TEST] Pass the gpio value is '{hex(gpio_value_int)}'")
+    else:
+        cocotb.log.error (f"[TEST] Fail the gpio value is :'{hex(gpio_value_int)}' expected {hex(expected)}")
 
 
